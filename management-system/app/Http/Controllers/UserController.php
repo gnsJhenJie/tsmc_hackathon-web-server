0bdbfca5dac03de0de9a0de38c8f1490fe75fcac
Jhen-Jie Hsieh <gnsjhenjie@g.ncu.edu.tw>
2022-10-21 14:03:58 +0000

    Update login intended url

--- management-system/app/Http/Controllers/UserController.php
+++ management-system/app/Http/Controllers/UserController.php
@@ -106,7 +106,7 @@ class UserController extends Controller
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
  
-            return redirect()->intended('dashboard');
+            return redirect()->intended('/');
         }
  
         return back()->withErrors([
